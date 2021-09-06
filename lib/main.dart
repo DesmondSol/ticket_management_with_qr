50c4f9fa49ffca14cb02b7ef59ee16696b7924c3
abiy <abeyabx123123@gmail.com>
2021-09-06 13:05:44 +0300

    hive imported

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:qr_generator_reader/screens/home.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(MyApp());
 }
 
